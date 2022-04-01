2d745f7eda03622f6aff54babbf15e7d4cc5001f
Patrick <peijian123228@163.com>
2022-04-01 15:39:23 +0800

    完成了权限功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Login from '../components/myLogin.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +21,8 @@ const router = new VueRouter({
             children: [
                 { path: '/welcome', component: Welcome },
                 { path: '/users', component: Users },
+                { path: '/rights', component: Rights },
+                { path: '/roles', component: Roles },
             ],
         },
     ],
